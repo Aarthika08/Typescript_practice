3f9ce3cadfe1a182894584ba4847d9d30ee3d27e
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2023-12-18 17:29:24 +0530

    Update and rename fizz-buzz to fizz-buzz.ts

---
+++ js to ts/fizz-buzz.ts
@@ -0,0 +1,15 @@
+const firstNumber: number = Number(prompt('Enter your 1st number '));
+
+if (firstNumber % 3 === 0) {
+    if (firstNumber % 5 === 0) {
+        console.log("Fizz Buzz");
+    } else {
+        console.log("Fizz");
+    }
+} else {
+    console.log("Buzz");
+}
+
+
+output
+[LOG]: "Fizz Buzz"

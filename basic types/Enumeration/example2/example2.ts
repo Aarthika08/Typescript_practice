2a8de74f152c0c8bd52afc4d8e6648cd19960f1d
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2023-12-18 14:09:34 +0530

    Create example2.ts

---
+++ basic types/Enumeration/example2/example2.ts
@@ -0,0 +1,46 @@
+enum Month {
+    Jan,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+}
+
+// Usage example:
+const currentMonth: Month = Month.Jan;
+console.log(currentMonth); // 0 (Jan)
+/*
+{
+  '0': 'Jan',
+  '1': 'Feb',
+  '2': 'Mar',
+  '3': 'Apr',
+  '4': 'May',
+  '5': 'Jun',
+  '6': 'Jul',
+  '7': 'Aug',
+  '8': 'Sep',
+  '9': 'Oct',
+  '10': 'Nov',
+  '11': 'Dec',
+  Jan: 0,
+  Feb: 1,
+  Mar: 2,
+  Apr: 3,
+  May: 4,
+  Jun: 5,
+  Jul: 6,
+  Aug: 7,
+  Sep: 8,
+  Oct: 9,
+  Nov: 10,
+  Dec: 11
+}
+*/

1ecf7a4a6f6b134ad3c77c550ce317d4dc0f667f
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2023-12-18 15:31:26 +0530

    Create example2.ts

---
+++ classes/getter_setter/example2.ts
@@ -0,0 +1,53 @@
+class Person {
+    private _age: number;
+    private _firstName: string;
+    private _lastName: string;
+
+    public get age(): number {
+        return this._age;
+    }
+
+    public set age(newAge: number) {
+        if (newAge <= 0 || newAge > 100) {
+            throw new Error("Invalid age");
+        }
+        this._age = newAge;
+    }
+
+    public get firstName(): string {
+        return this._firstName;
+    }
+
+    public set firstName(newFirstName: string) {
+        if (!newFirstName) {
+            throw new Error("Invalid first name");
+        }
+        this._firstName = newFirstName;
+    }
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+
+    public set lastName(newLastName: string) {
+        if (!newLastName) {
+            throw new Error("Invalid last name");
+        }
+        this._lastName = newLastName;
+    }
+
+    public getFullName(): string {
+        return `${this._firstName} ${this._lastName}`;
+    }
+}
+
+const person = new Person();
+person.age = 10;
+person.firstName = 'aaa';
+person.lastName = 'bbb';
+
+console.log(person.age); // Output: 10
+console.log(person.firstName); // Output: aaa
+console.log(person.lastName); // Output: bbb
+
+console.log(person.getFullName()); // Output: aaa bbb

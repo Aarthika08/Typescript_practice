fb7802314c5acb238535f7d40512e75ee64417ed
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2023-12-18 13:55:42 +0530

    Create array-filter.js

---
+++ basic types/arrays/filter/array-filter.js
@@ -0,0 +1,6 @@
+var isBigEnough = function (element) {
+    return element >= 10;
+};
+var passed = [12, 5, 8, 130, 44].filter(isBigEnough);
+console.log("Test Value: " + passed);
+var scores = ['Programming', 5, 'Software Design', 4];

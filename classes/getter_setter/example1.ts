930cf14e8ac476bd3c7d29254eaba6960570ce5a
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2023-12-18 15:39:17 +0530

    Update example1.ts

--- classes/getter_setter/example1.ts
+++ classes/getter_setter/example1.ts
@@ -1,29 +1,42 @@
 class Person {
-  private _age: number;
-  private fname: string;
-  private lname: string;
+    private _age: number;
+    private _firstName: string;
+    private _lastName: string;
 
-  public get age(): number {
-    return this._age;
-  }
-
-  public set age(tAge: number) {
-    if (tAge <= 0 || tAge > 100) {
-      throw new Error("Invalid age");
+    public get age(): number {
+        return this._age;
     }
-    this._age = tAge;
-  }
 
-  public getFullName(): string {
-    return `${this.fname} ${this.lname}`;
-  }
+    public set age(newAge: number) {
+        if (newAge <= 0 || newAge > 100) {
+            throw new Error("Invalid age");
+        }
+        this._age = newAge;
+    }
+
+    public get fullName(): string {
+        return `${this._firstName} ${this._lastName}`;
+    }
+
+    public set firstName(newFirstName: string) {
+        if (!newFirstName) {
+            throw new Error("Invalid first name");
+        }
+        this._firstName = newFirstName;
+    }
+
+    public set lastName(newLastName: string) {
+        if (!newLastName) {
+            throw new Error("Invalid last name");
+        }
+        this._lastName = newLastName;
+    }
 }
 
-let person = new Person();
+const person = new Person();
 person.age = 10;
-person.fname = 'aaa';
-person.lname = 'bbb';
+person.firstName = 'aaa';
+person.lastName = 'bbb';
 
 console.log(person.age); // Output: 10
-console.log(person.getFullName()); // Output: aaa bbb
-___________________________________________________________________________________________________________________________________________________________
+console.log(person.fullName); // Output: aaa bbb

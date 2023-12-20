b7ce55b055bf43fc60ea943fddd5f72c735940be
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2023-12-20 13:59:43 +0530

    Update proggram-2.ts

--- Business Logic/20-12-23/longest-sequence/proggram-2.ts
+++ Business Logic/20-12-23/longest-sequence/proggram-2.ts
@@ -12,99 +12,111 @@
 
 import * as readlineSync from 'readline-sync';//to get user input
 
-
-class Product{
+class Product {
     constructor(public id: number, public name: string, public price: number) {}
 }
-class ProductCalculator{
+
+class ProductCalculator {
     private products: Product[] = [];
 
-    addProduct(id: number, name: string, price: number): void{
-        const product=new Product(id,name,price);
+    addProduct(id: number, name: string, price: number): void {
+        const product = new Product(id, name, price);
         this.products.push(product);
     }
 
-    calculateMaximumProduct():{maxProduct: number, productIds: number[], productNames: string[]}{
-       let maxProduct =0;
-       let maxProductIds: number[] = [];
-       let maxProductNames: string[] = [];
+    calculateMaximumProduct(): { maxProduct: number, productIds: number[], productNames: string[] } {
+        let maxProduct = 0;
+        let maxProductIds: number[] = [];
+        let maxProductNames: string[] = [];
 
-        for(let i=0;i<=this.products.length-2;i++){
-                const product=this.products[i].price*this.products[i+1].price;
-                // max_product = Math.max(max_product, product)
-                if (product > maxProduct) {
-                    maxProduct = product;
-                    maxProductIds = [this.products[i].id, this.products[i + 1].id];
-                    maxProductNames = [this.products[i].name, this.products[i + 1].name];
+        for (let i = 0; i <= this.products.length - 2; i++) {
+            const product = this.products[i].price * this.products[i + 1].price;
 
+            if (product > maxProduct) {
+                maxProduct = product;
+                maxProductIds = [this.products[i].id, this.products[i + 1].id];
+                maxProductNames = [this.products[i].name, this.products[i + 1].name];
+            }
         }
-        // return max_product;
 
+        return { maxProduct, productIds: maxProductIds, productNames: maxProductNames };
     }
-    return { maxProduct, productIds: maxProductIds, productNames: maxProductNames };
 
+
+
+    calculatingLongestSequence(): { maxLength: number } {
+        let currentLength = 1;
+        let maxLength = 1;
+        let currentStartIndex = 0;
+    
+        let longestSequence: { productIds: number[], productNames: string[], productPrice: number[] } = {
+            productIds: [this.products[0].id],
+            productNames: [this.products[0].name],
+            productPrice: [this.products[0].price],
+        };
+    
+        for (let i = 1; i < this.products.length; i++) {
+            if (this.products[i].price > this.products[i - 1].price) {
+                currentLength++;
+                longestSequence.productIds.push(this.products[i].id);
+                longestSequence.productNames.push(this.products[i].name);
+                longestSequence.productPrice.push(this.products[i].price);
+            } else {
+                currentLength = 1;
+                longestSequence = {
+                    productIds: [this.products[i].id],
+                    productNames: [this.products[i].name],
+                    productPrice: [this.products[i].price],
+                };
+            }
+    
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+                currentStartIndex = i - currentLength + 1;
+            }
+        }
+    
+        console.log("Products in the longest ascending sequence:");
+        for (let i = currentStartIndex; i < currentStartIndex + maxLength; i++) {
+            console.log(`ID: ${this.products[i].id}, Name: ${this.products[i].name}, Price: ${this.products[i].price}`);
+        }
+    
+        return {
+            // productIds: longestSequence.productIds,
+            // productNames: longestSequence.productNames,
+            // productPrice: longestSequence.productPrice,
+            maxLength
+        };
+    }
+    
 }
-
-calculating_longest_sequence():{productIds: number[], productNames: string[],productprice :number[],maxcount:number}{
-    let maxProductIds:number[] = [];
-    let maxProductNames:string[] = [];
-    let maxProductprice:number[] = [];
-    let productPairs: { id: number, name: string, price: number }[] = [];
-let count:number=0;
-
-
-     for(let i=0;i<=this.products.length-2;i++){          
-
-             if (this.products[i].price < this.products[i+1].price) {
-               
-                  maxProductIds.push(this.products[i].id,this.products[i+1].id);
-                 maxProductNames.push(this.products[i].name,this.products[i+1].name);
-                 maxProductprice.push(this.products[i].price,this.products[i+1].price);
-
-                // maxProductIds=[this.products[i].id,this.products[i+1].id];
-                //  maxProductNames=[this.products[i].name,this.products[i+1].name];
-                //  maxProductprice=[this.products[i].price,this.products[i+1].price];
-count+=1
-             }
- }
- return { productIds: maxProductIds, productNames: maxProductNames ,productprice:maxProductprice,maxcount:count};
-
-}
-
-}
-
-
-
-let productcalculator=new ProductCalculator();
-const Numberofproduct=Number(readlineSync.question("Enter Number of product:"));
-for(let i=0;i<Numberofproduct;i++)
-{
+let productcalculator = new ProductCalculator();
+const Numberofproduct = Number(readlineSync.question("Enter Number of product:"));
+for (let i = 0; i < Numberofproduct; i++) {
     const id = Number(readlineSync.question("Enter your id: "));
     const Name = readlineSync.question("Enter your Name: ");
     const price = Number(readlineSync.question("Enter your price: "));
-    // inputvalue.push(Number(readlineSync.question("Enter your value: ")))
     // Validate that id and price are valid numbers
     if (isNaN(id) || isNaN(price)) {
-        console.log('Invalid input. Please enter valid numeric values for ID and Price.');break;
+        console.log('Invalid input. Please enter valid numeric values for ID and Price.');
+        break;
     }
-    productcalculator.addProduct(id,Name,price);
+    productcalculator.addProduct(id, Name, price);
 }
-
-// const { maxProduct, productIds, productNames } = productcalculator.calculateMaximumProduct();
-
-// console.log(`The maximum product of adjacent prices: ${maxProduct}`);
-// -------
 
 const result = productcalculator.calculateMaximumProduct();
 console.log(`The maximum product of adjacent prices: ${result.maxProduct}`);
 console.log(`Product IDs contributing to the maximum product: ${result.productIds.join(', ')}`);
 console.log(`Product Names contributing to the maximum product: ${result.productNames.join(', ')}`);
 
-const result2 = productcalculator.calculating_longest_sequence();
-console.log(`Product count contributing to the maximum product: ${result2.maxcount}`);
-console.log(`Product IDs contributing to the maximum product: ${result2.productIds.join(',')}`);
-console.log(`Product Names contributing to the maximum product: ${result2.productNames.join(', ')}`);
-console.log(`Product price contributing to the maximum product: ${result2.productprice.join(', ')}`);
+const result2 = productcalculator.calculatingLongestSequence();
+console.log(`Length of the longest ascending sequence: ${result2.maxLength}`);
+// console.log("Products in the longest ascending sequence:");
+// for (let i = 0; i < result2.productIds.length; i++) {
+//     console.log(`ID: ${result2.productIds[i]}, Name: ${result2.productNames[i]}, Price: ${result2.productPrice[i]}`);
+// }
+
+
 
 output
 
